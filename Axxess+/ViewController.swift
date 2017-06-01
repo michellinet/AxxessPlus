885be0dbcbbd64093e5ba016f4d699adaa9d7456
michellinet <michellinet@procore.com>
2017-06-01 09:34:57 -0700

    Add some merchants, create outlets to UI.

--- Axxess+/ViewController.swift
+++ Axxess+/ViewController.swift
@@ -7,19 +7,42 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
-
+    
+    var merchantList: [AxxessMerchant]!
+    
+    func setupMerchantList() {
+        let merchantOne = AxxessMerchant(name: "TAP Thai Cuisine", address: "3130 State St., Santa Barbara, CA 93105", oneTimeDeal: "Buy One Entree, Get One Free.", continualDeal: "Save 10% Thereafter.")
+        let merchantTwo = AxxessMerchant(name: "Live Oak Cafe", address: "2220 Bath St., Santa Barbara, CA 93105", oneTimeDeal: "Buy One Entree, Get One Free.", continualDeal: "Save 10% Thereafter.")
+        let merchantThree = AxxessMerchant(name: "Los Agaves - De La Vina", address: "2911 De La Vina St., Santa Barbara, CA 93105", oneTimeDeal: "Buy One Entree, Get One Free.", continualDeal: "Free Fountain Drink with Entree Purchase Thereafter.")
+        merchantList = []
+        merchantList.append(contentsOf: [merchantOne,merchantTwo, merchantThree])
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupMerchantList()
+    }
+    @IBAction func tellMeWhatToEatPressed(_ sender: Any) {
+        let randomIndex = Int(arc4random() % 3)
+        let randomMerchant = merchantList[randomIndex]
+        
+        merchantName.text = randomMerchant.name
+        merchantAddress.text = randomMerchant.address
+        merchantOneTimeDeal.text = randomMerchant.oneTimeDeal
+        merchantContinualDeal.text = randomMerchant.continualDeal
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    @IBOutlet weak var merchantName: UILabel!
 
+    @IBOutlet weak var merchantAddress: UILabel!
 
+    @IBOutlet weak var merchantOneTimeDeal: UILabel!
+
+    @IBOutlet weak var merchantContinualDeal: UILabel!
 }
 
