5f434550d72cb99a388a221d6cfed0faf6f95523
michellinet <michellinet@procore.com>
2017-05-31 20:25:42 -0700

    Set up UI!

---
+++ Axxess+/AxxessMerchant.swift
@@ -0,0 +1,24 @@
+//
+//  AxxessMerchant.swift
+//  Axxess+
+//
+//  Created by Michelline Tran on 5/31/17.
+//  Copyright © 2017 Michelline Tran. All rights reserved.
+//
+
+import Foundation
+
+class AxxessMerchant {
+    let name: String
+    let address: String
+    let oneTimeDeal: String
+    let continualDeal: String
+    
+    init(name: String, address: String, oneTimeDeal: String, continualDeal: String) {
+        self.name = name
+        self.address = address
+        self.oneTimeDeal = oneTimeDeal
+        self.continualDeal = continualDeal
+    }
+    
+}
