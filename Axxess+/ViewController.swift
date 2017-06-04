b6f0fa005618b03c321ad2061573bf8c5344a2e8
michellinet <michellinet@procore.com>
2017-06-03 21:11:17 -0700

    Map shows pin for merchant location.

--- Axxess+/ViewController.swift
+++ Axxess+/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController, MKMapViewDelegate{
+class ViewController: UIViewController, MKMapViewDelegate {
     
     var merchantList: [AxxessMerchant]!
     
@@ -25,6 +25,7 @@ class ViewController: UIViewController, MKMapViewDelegate{
     override func viewDidLoad() {
         super.viewDidLoad()
         setupMerchantList()
+        merchantMapView.delegate = self
     }
     @IBAction func tellMeWhatToEatPressed(_ sender: Any) {
         let randomIndex = Int(arc4random() % 3)
@@ -35,13 +36,41 @@ class ViewController: UIViewController, MKMapViewDelegate{
         merchantOneTimeDeal.text = randomMerchant.oneTimeDeal
         merchantContinualDeal.text = randomMerchant.continualDeal
         
+        setupMKView(address: randomMerchant.address, MKView: merchantMapView)
     }
 
     @IBOutlet weak var merchantName: UILabel!
-
+    
     @IBOutlet weak var merchantAddress: UILabel!
-
+    
     @IBOutlet weak var merchantMapView: MKMapView!
+    
+    var annotation: MKPointAnnotation?
+    
+    func setupMKView (address: String, MKView: MKMapView) {
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(address) { (placemarkArray, error) in
+            if (error != nil) {
+                return
+            }
+            
+            if let placemark = placemarkArray?.first, let location = placemark.location {
+                //MKView.setCenter(location.coordinate, animated: true)
+                let region = MKCoordinateRegion(center: location.coordinate, span: MKCoordinateSpanMake(1/69, 1/69))
+                MKView.setRegion(region, animated: true)
+                
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = location.coordinate
+                MKView.showAnnotations([annotation], animated: true)
+            }
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let pin = MKPinAnnotationView()
+        pin.pinTintColor = UIColor(red: 77/255.0, green: 21/255.0, blue: 186/255.0, alpha: 1)
+        return pin
+    }
     
     @IBOutlet weak var merchantOneTimeDeal: UILabel!
 
