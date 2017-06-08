7feeeed20559dac425dfaf2c514c41ab53cc3819
Michelline Tran <michellinet@procore.com>
2017-06-08 13:32:09 -0700

    AxxessMerchant conforms to NSCoding.

--- Axxess+/AxxessMerchant.swift
+++ Axxess+/AxxessMerchant.swift
@@ -8,17 +8,37 @@
 
 import Foundation
 
-class AxxessMerchant {
+class AxxessMerchant: NSCoding {
     let name: String
     let address: String
     let oneTimeDeal: String
     let continualDeal: String
-    
+
     init(name: String, address: String, oneTimeDeal: String, continualDeal: String) {
         self.name = name
         self.address = address
         self.oneTimeDeal = oneTimeDeal
         self.continualDeal = continualDeal
     }
-    
+
+    //MARK: NSCoding
+
+    required convenience init?(coder decoder: NSCoder) {
+        guard let name = decoder.decodeObject(forKey: "name") as? String,
+            let address = decoder.decodeObject(forKey: "address") as? String,
+            let oneTimeDeal = decoder.decodeObject(forKey: "oneTimeDeal") as? String,
+            let continualDeal = decoder.decodeObject(forKey: "continualDeal") as? String
+            else {return nil}
+
+        self.init(name: name, address: address, oneTimeDeal: oneTimeDeal, continualDeal: continualDeal)
+
+    }
+
+    func encode(with coder: NSCoder) {
+        coder.encode(self.name, forKey: "name")
+        coder.encode(self.address, forKey: "address")
+        coder.encode(self.oneTimeDeal, forKey: "oneTimeDeal")
+        coder.encode(self.continualDeal, forKey: "continualDeal")
+    }
+
 }
