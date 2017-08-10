5c2c8d4fa9c70e65950f3045e62f85eb9e19e12d
michellinet <michellinet@procore.com>
2017-08-09 22:18:52 -0700

    Set up fetchedResultsController.

--- Axxess+/DirectoryTableViewController.swift
+++ Axxess+/DirectoryTableViewController.swift
@@ -9,10 +9,24 @@
 import UIKit
 import CoreData
 
-class DirectoryTableViewController: UITableViewController {
+class DirectoryTableViewController: UITableViewController, NSFetchedResultsControllerDelegate {
     private var merchants: [Merchant] = []
     
     var container: NSPersistentContainer? = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer
+    
+    fileprivate var fetchedResultsController: NSFetchedResultsController<Merchant>!
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        
+//        do {
+//            try fetchedResultsController.performFetch()
+//        } catch {
+//            fatalError("Failure to perform fetch: \(error)")
+//        }
+        
+        tableView.reloadData()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,32 +36,40 @@ class DirectoryTableViewController: UITableViewController {
 
         tableView.rowHeight = UITableViewAutomaticDimension
         tableView.estimatedRowHeight = 44
-
-        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
-        let managedContext = appDelegate.persistentContainer.viewContext
-        let fetchRequest = NSFetchRequest<Merchant>(entityName: "Merchant")
-        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "name", ascending: true, selector: #selector(NSString.localizedCaseInsensitiveCompare(_:)))]
+        
+        // Create Fetch Request
+        let fetchRequest: NSFetchRequest<Merchant> = Merchant.fetchRequest()
+        
+        // Configure Fetch Request
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "name", ascending: true)]
+        
+        // Create Fetched Results Controller
+        fetchedResultsController = NSFetchedResultsController(
+            fetchRequest: fetchRequest,
+            managedObjectContext: self.container!.viewContext,
+            sectionNameKeyPath: nil, cacheName: nil
+        )
+        
+        // Configure Fetched Results Controller
+        fetchedResultsController.delegate = self
 
         do {
-            merchants = try managedContext.fetch(fetchRequest)
+           try fetchedResultsController.performFetch()
         } catch let error as NSError {
-            print("Couldn't fetch. \(error), \(error.userInfo)")
+            print("Couldn't fetch. \(error), \(error.localizedDescription)")
         }
     }
     
-    //TODO: refactor & set up FRC
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        tableView.reloadData()
-    }
-    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let merchants = fetchedResultsController.fetchedObjects else { return 0 }
         return merchants.count
     }
     
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let merchant = merchants[indexPath.row]
+        let merchant = fetchedResultsController.object(at: indexPath)
         let cell = tableView.dequeueReusableCell(withIdentifier: "Merchant", for: indexPath) as! DirectoryTableViewCell
+        
         cell.name.text = merchant.value(forKeyPath: "name") as? String
         cell.address.text = merchant.value(forKeyPath: "address") as? String
         if merchant.checkAvailableOneTimeDeals() == 0 {
@@ -55,19 +77,48 @@ class DirectoryTableViewController: UITableViewController {
         } else {
             cell.dealCount.text = "\(merchant.checkAvailableOneTimeDeals())"
         }
+        
+        
+        
         return cell
     }
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         performSegue(withIdentifier: "detailSegue", sender: nil)
+        
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "detailSegue",
             let nextScene = segue.destination as? MerchantDetailViewController,
             let indexPath = tableView.indexPathForSelectedRow {
-            let selectedMerchant = merchants[indexPath.row]
+            let selectedMerchant = fetchedResultsController.object(at: indexPath)
             nextScene.currentMerchant = selectedMerchant
+        }
+    }
+    
+    //MARK: NSFetchedResultsControllerDelegate Methods
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        switch (type) {
+        case .update:
+            if let indexPath = indexPath, let merchant = anObject as? Merchant {
+                let cell = tableView.cellForRow(at: indexPath) as! DirectoryTableViewCell
+                if merchant.checkAvailableOneTimeDeals() == 0 {
+                   cell.containerStackView.removeArrangedSubview(cell.dealIndicatorView)
+                } else {
+                    cell.dealCount.text = "\(merchant.checkAvailableOneTimeDeals())"
+                }
+            }
+        default:
+            print("")
         }
     }
 }
